b03004f8b004a671bf5b6450e9115d657ba06582
shajeeh3334 <17ncs09@meaec.edu.in>
2020-11-21 13:45:38 +0530

    First commit : first version

---
+++ va.js
@@ -0,0 +1,19 @@
+
+// function ValidateEmail(mail) 
+// {
+//  if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(myForm.emailAddr.value))
+//   {
+//     return (true)
+//   }
+//   if else  (Email.value==""||password =="") {
+//       alert("please fill email")
+//       return (false)
+      
+//   }
+
+
+  
+//     alert("You have entered an invalid email address!")
+//     return (false)
+// }
+
